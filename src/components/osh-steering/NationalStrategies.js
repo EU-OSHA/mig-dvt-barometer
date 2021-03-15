54d546a3941cc9c642218cfa1aa964e0c8cfa448
dmarina <aprado@bilbomatica.es>
2021-03-15 10:49:50 +0100

    Finished adding react in NationalStrategies.js

--- src/components/osh-steering/NationalStrategies.js
+++ src/components/osh-steering/NationalStrategies.js
@@ -2,16 +2,93 @@ import React, { Component } from 'react';
 import ReactHtmlParser from 'react-html-parser';
 import { Link } from 'react-router-dom';
 
+const API_ADDRESS = 'http://89.0.4.28:8080/barometer-data-server/api';
+
 class NationalStrategies extends Component
 {
+	constructor(props){
+		super(props);
+		this.state = { countries: [], countriesFiltered: [], alphabetFiltered: [], alphabet: []};
+	}
+
+	createLetterNavigation = () => {
+		var letter = "";
+		var index = "";
+		var temporalArray = [];
+		for(var i=0;i<this.state.countries.length;i++){
+			if(this.state.countries[i] != null){
+				letter = this.state.countries[i].name.charAt(0);
+				index = temporalArray.indexOf(letter);
+
+				if(index === -1){
+					temporalArray.push(letter);
+				}
+			}
+		}
+		this.setState({alphabet: temporalArray});
+		this.state.alphabet.sort();
+	}
+
+	componentDidMount(){
+        fetch(`${API_ADDRESS}/countries/getCountriesStrategiesPage?page=STRATEGY`)
+            .then(response => response.json())
+            .then(json => {
+				this.setState({countries: json.resultset});
+				this.filterCountries();
+				this.createLetterNavigation();
+            })
+            .catch(error => console.log(error.message));
+	}
+
+	resetFilter = () => {
+		this.setState({alphabetFiltered: [], countriesFiltered: this.state.countries});
+	}
+
+	addLetter = (letter) => () => {
+		var temporalArray = this.state.alphabetFiltered;
+		if (temporalArray.indexOf(letter) == -1)
+        {
+			temporalArray.push(letter); 
+        }
+        else
+        {
+			temporalArray.splice(temporalArray.indexOf(letter), 1);
+        }
+		
+		this.setState({alphabetFiltered: temporalArray});
+		this.filterCountries();
+	}
+
+	filterCountries = () => {
+		if(this.state.alphabetFiltered.length === 0){
+			this.setState({countriesFiltered: this.state.countries});
+			console.log('No countries filtered. Showing all countries');
+		}else{
+			var temporalArray = [];
+			this.state.countries.filter(country => this.state.alphabetFiltered.indexOf(country.name.charAt(0)) != -1).map(filteredCountry => {
+				temporalArray.push(filteredCountry);
+			});
+			this.setState({countriesFiltered: temporalArray});
+		}
+	}
+
+	filterAlphabet = (letter) => {
+		if (this.state.alphabetFiltered.indexOf(letter) == -1)
+        {
+			return "";
+		}else{
+			return "active";
+		}
+	}
+
 	render()
 	{
 		return(
 			<div>
-				<section data-ng-if="state.current.name != 'home'" id="not-home-cover" className="advice--icon--block advice--block-not-home background-main-light container-fluid section--page">
+				<section id="not-home-cover" className="advice--icon--block advice--block-not-home background-main-light container-fluid section--page">
 					<div className="container horizontal-nopadding">
 						<div className="text-advice left-text col-md-8 col-sm-8 col-xs-12  nopadding">
-						<h1 clclassNameass="main-color left-text">{this.props.literals.L22007}</h1>
+						<h1 className="main-color left-text">{this.props.literals.L22007}</h1>
 						<p>{this.props.literals.L22038}</p>
 						<span>{this.props.literals.L20704} </span> 
 						<span>
@@ -27,11 +104,16 @@ class NationalStrategies extends Component
 				<section className="filter--letter--section container">
 					<ul className="filter--letter--list">
 						{/* FILTER BY LETTER */}
-						<li data-ng-repeat="letter in alphabet" className="filter-letter-item">
-							<a href data-ng-click="addLetter(letter)" data-ng-bind="letter" data-ng-class="{'active': countryFilter.indexOf(letter)!=-1}"></a>
-						</li>
+						{
+							this.state.alphabet.map((letter, index) => (
+								<li className="filter-letter-item" key={index}>
+									<a onClick={this.addLetter(letter)} className={this.filterAlphabet(letter)}>{letter}</a>
+								</li>
+							))
+						}
+
 						<li className="reset-item">
-							<Link data-ng-click="resetFilter()" class="btn-default btn-main-color btn-reset">{this.props.literals.L20628}</Link>
+							<Link onClick={this.resetFilter} className="btn-default btn-main-color btn-reset">{this.props.literals.L20628}</Link>
 						</li>
 					</ul>
 				</section>
@@ -39,20 +121,28 @@ class NationalStrategies extends Component
 				<div className="line background-main-light"></div>
 				<section className="national--card--section">
 					<div className="card--grid xxs-w1 xs-w3 md-w3 w4 container">
-					<div data-ng-repeat="country in countries" className="card--block--rounded national--card--item">
-						<div className="resources-item">
-						<p className="nopadding additional-img text-center">
-							<img  className="flags--wrapper" ng-src="../../style/img/flag/{{::country.country_code.toLowerCase()}}.png" />
-						</p>
-						<h2 className="text-center" data-ng-bind="i18nLiterals['L'+country.country]"></h2>
-						<p className="btn--block-full left-text">
-							<Link to="country-profile" className="btn-default btn-main-color btn-full" title="{this.props.literals.L20626}">{this.props.literals.L20626}</Link>
-						</p>
-						<p className="btn--block-full left-text">
-							<Link to="EU-challenges-response" className="btn-default btn-main-color btn-full" title="{this.props.literals.L20625}" >{this.props.literals.L20625}</Link>
-						</p>
-						</div>
-					</div>
+						{
+							this.state.countriesFiltered.map((country,index) => (
+								<div className="card--block--rounded national--card--item" key={index}>
+									<div className="resources-item">
+										<p className="nopadding additional-img text-center">
+											<img className="flags--wrapper" src={require(`../../style/img/flag/at.png`)} alt={country.name} />
+											{/* <img className="flags--wrapper" src={require(`../../style/img/flag/${country.code.toLowerCase()}.png`)} alt={country.name} /> */}
+											{/* <img className="flags--wrapper" ng-src="../../style/img/flag/{{::country.country_code.toLowerCase()}}.png" /> */}
+										</p>
+										<h2 className="text-center">{country.name}</h2>
+										<p className="btn--block-full left-text">
+											<Link to={"/osh-steering/country-profile/basic-information/"+country.code} className="btn-default btn-main-color btn-full" title={this.props.literals.L20626}>{this.props.literals.L20626}</Link>
+											{/* <Link to="country-profile({pIndicator: 'basic-information', pCountry1:country.country_code, pCountry2: 0})" className="btn-default btn-main-color btn-full" title={this.props.literals.L20626}>{this.props.literals.L20626}</Link> */}
+										</p>
+										<p className="btn--block-full left-text">
+											<Link to={"/osh-steering/eu-challenges-response/"+country.code} className="btn-default btn-main-color btn-full" title={this.props.literals.L20625} >{this.props.literals.L20625}</Link>
+											{/* <Link to="EU-challenges-response({pCountry:country.country_code})" className="btn-default btn-main-color btn-full" title={this.props.literals.L20625} >{this.props.literals.L20625}</Link> */}
+										</p>
+									</div>
+								</div>
+							))
+						}
 					</div>
 				</section>
 
